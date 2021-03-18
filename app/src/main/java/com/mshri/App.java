1562e3d418b2fa66c05c5edbd1612eade738dc8d
Manvendra Shrinetra <mshrinetra@gmail.com>
2021-03-18 23:18:32 +0530

    Import to eclipse

--- app/src/main/java/com/mshri/App.java
+++ app/src/main/java/com/mshri/App.java
@@ -5,7 +5,7 @@ package com.mshri;
 
 public class App {
     public String getGreeting() {
-        return "Hello World!";
+        return "Hello World! \n\t Imported in eclipse";
     }
 
     public static void main(String[] args) {
